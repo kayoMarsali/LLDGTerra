b6a0722c32028e64461a3310e4c0c18c38fb49d2
KayoMarsali <brandonsmithies900@hotmail.com>
2026-04-02 12:22:27 -0700

    add imgui and implement with rlImGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include <print>
 #include <raylib.h>
+#include <imgui.h>
+#include <rlImGui.h>
 
 int main() {
 
-	InitWindow(1280, 720, "LLGD Terra");
+	InitWindow(1920, 1080, "LLGD Terra");
+
+	rlImGuiSetup(true);
+
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.FontGlobalScale = 1.5;
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
+
+
+		#pragma region imgui
+		rlImGuiBegin();
+
+		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport()->ID);
+		ImGui::PopStyleColor(2);
+
+
+
+		rlImGuiEnd();
+		#pragma endregion imgui
+
 		EndDrawing();
 	}
 
+	rlImGuiShutdown();
 
 	CloseWindow();
 	return 0;
